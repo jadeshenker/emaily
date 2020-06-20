9a9b206d264b6bb8aaf60046aee4cc64bfd2236e
Jade Shenker <jsarahshenker@gmail.com>
2020-06-20 14:28:31 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+/* dynamically figure out which port to listen to 
+ * heroku's opportunity to pass us some runtime configuration 
+ * have to wait until the last second to figure out what the port is 
+ * look at underlying env and figure out what port to use 
+ * variable will not be defined if we're in development environment 
+ */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
