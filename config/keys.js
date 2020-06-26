55d46bde04482d17a55d797e91a9ea6e6b2d89a5
Jade Shenker <jsarahshenker@gmail.com>
2020-06-26 09:51:40 -0700

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - determine WHICH set of credentials should be returned
+
+if(process.env.NODE_ENV === 'production') {
+    //we are in production, return prod keys 
+    module.exports = require('./prod');
+} else {
+    //we are in development, return dev keys 
+    module.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'friend'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 /* dynamically figure out which port to listen to 
  * heroku's opportunity to pass us some runtime configuration 
